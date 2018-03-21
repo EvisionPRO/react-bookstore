8dd5fbd5f1dff03ed3e36d1c8b96b12151da2bd7
Jarek Sliz <jarek@builderdesigns.com>
2018-03-20 22:31:33 -0500

    added header and build increment and decrease method

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Link} from 'react-router';
 
 class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      number: '0'
+    };
+    // this.IncrementNumber = this.IncrementItem.bind(this);
+    // this.DecreaseNumber = this.DecreaseItem.bind(this);
+  }
+
+  IncrementNumber() {
+    const number = this.state.number + 1;
+    this.setState({ number: number});
+  }
+  DecreaseNumber() {
+    console.log('jarek', this.state.number);
+    this.setState({ number: this.state.number - 1 });
+  }
+
   render() {
+
     return (
-      <div className="jumbotron">
-        <h1>Here is my First Bookstore</h1>
-        <p>Here is a few information about a new react and redux bookstore</p>
-        <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
+      <div>
+        <div className="jumbotron">
+          <h1>Here is my First Bookstore</h1>
+          <p>Here is a few information about a new react and redux bookstore</p>
+          <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
+        </div>
+        <div className="flexcontainer">
+          <i className="btn btn-primary" onClick={this.DecreaseNumber}>-</i>
+          <h3>{this.state.number}</h3>
+          <i className="btn btn-primary" onClick={this.IncrementNumber}>+</i>
+        </div>
       </div>
     );
   }
 }
+
 
 export default HomePage;
